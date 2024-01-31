c91fc9f11c3cfe605a28e86680be7ae20bb2f7f8
brutchjw <brutchjw@rose-hulman.edu>
2024-01-31 14:41:36 -0500

    added wrong file

--- testFeatureExtract.m
+++ testFeatureExtract.m
@@ -1,4 +1,13 @@
-ds = imageDatastore("LogoDet-3K (1)/LogoDet-3K/Transportation/", ...
+clear;
+clc;
+
+%jadon's filepath note: I am using the images after running that python
+%script, and I would imagine that your accuracy will drop/be lower than mine 
+% if you haven't run that script yet due to multiple labels for the same class
+
+filePath = "TransportationCleaned/";
+
+ds = imageDatastore(filePath, ...
     FileExtensions=[".jpg"], ...
     IncludeSubfolders=true, ...
     LabelSource="foldernames");
@@ -6,28 +15,72 @@ nImages = numel(ds.Files);
 images = ds.Files;
 labels = ds.Labels;
 
-imagepaths = strings(nImages,1);
-for i = 1:nImages
-    imagepaths(i) = string(images(i));
-end
 
 %% Generate training, validation, and test sets
 rng("default");
-shuffledIndices = randperm(nImages);
-idx = floor(0.6 * length(shuffledIndices) );
 
-trainingIdx = 1:idx;
-trainingData = imagepaths(shuffledIndices(trainingIdx),:);
-trainLabels = labels(shuffledIndices(trainingIdx),:);
+% Get a list of subfolders in the main folder
+subfolders = dir(filePath);
+subfolders = subfolders([subfolders.isdir]); % Keep only directories
+subfolders = subfolders(~ismember({subfolders.name}, {'.', '..'})); % Remove '.' and '..'
 
+trainingPercentage = 60;
+validationPercentage = 10;
+testingPercentage = 30;
 
-validationIdx = idx+1 : idx + 1 + floor(0.1 * length(shuffledIndices) );
-validationData = imagepaths(shuffledIndices(validationIdx),:);
-validationLabels = labels(shuffledIndices(validationIdx),:);
+trainingData = {};
+validationData = {};
+testingData = {};
 
-testIdx = validationIdx(end)+1 : length(shuffledIndices);
-testData = imagepaths(shuffledIndices(testIdx),:);
-testLabels = labels(shuffledIndices(testIdx),:);
+trainingLabels = {};
+validationLabels = {};
+testingLabels = {};
+
+for i = 1:length(subfolders)
+    subfolder = fullfile(filePath, subfolders(i).name);
+    
+    % Get a list of files in the current subfolder
+    imgFiles = dir(fullfile(subfolder, '*.jpg'));
+    
+    % Randomly shuffle the list of files
+    perm = randperm(length(imgFiles));
+
+    % Calculate the number of files for each set
+    numFiles = length(imgFiles);
+    numTraining = round(trainingPercentage/100 * numFiles);
+    numValidation = round(validationPercentage/100 * numFiles);
+    numTesting = numFiles - numTraining - numValidation;
+    
+    % Divide the files into training, validation, and testing sets
+    
+    for j = 1:numTraining
+        trainingData = [trainingData; fullfile(subfolder, imgFiles(perm(j)).name)'];
+        trainingLabels = [trainingLabels; labels(perm(j))];
+    end
+
+    for j = numTraining+1:numTraining+numValidation
+        validationData = [validationData; fullfile(subfolder, imgFiles(perm(j)).name)'];
+        validationLabels = [validationLabels; labels(perm(j))];
+    end
+
+    for j = numTraining+numValidation+1:numFiles
+        testingData = [testingData; fullfile(subfolder, imgFiles(perm(j)).name)'];
+        testingLabels = [testingLabels; labels(perm(j))];
+    end
+
+    disp(['Processing subfolder: ' subfolders(i).name]);
+    disp(['  Training set: ' num2str(numTraining) ' files']);
+    disp(['  Validation set: ' num2str(numValidation) ' files']);
+    disp(['  Testing set: ' num2str(numTesting) ' files']);
+
+end
+
+%% Generate datastores
+imgTrain = imageDatastore(trainingData);
+
+imgValidation = imageDatastore(validationData);
+
+imgTest = imageDatastore(testingData);
 
 %% Saving off data
 save('featExtractData.mat');
@@ -35,26 +88,21 @@ save('featExtractData.mat');
 %% Load Data
 load("featExtractData.mat")
 
-%% Generate datastores
-imgTrain = imageDatastore(trainingData, "Labels",trainLabels);
-
-imgValidation = imageDatastore(validationData,"Labels",validationLabels);
-
-imgTest = imageDatastore(testData,"Labels",testLabels);
-
-YTrain = imgTrain.Labels;
-YTest = imgTest.Labels;
-
-%% Feature Extraction
+%% Load net and augment inputs 
 
 net = resnet101;
 
 inputSize = net.Layers(1).InputSize;
 
+%% Augment Inputs
+
+%you could experiment here with different augmentations maybe?
 imageAugmenter = imageDataAugmenter('RandXTranslation', [-20 20], 'RandYTranslation', [-20 20], 'RandRotation', [-180 180], 'RandScale', [0.5 2]);
 augimdsTrain = augmentedImageDatastore(inputSize(1:2), imgTrain, 'DataAugmentation', imageAugmenter);
 augimdsValidation = augmentedImageDatastore(inputSize(1:2), imgValidation);
 augimdsTest = augmentedImageDatastore(inputSize(1:2), imgTest);
+
+%% Feature Extraction with global pooling layer: pool5 features
 
 layer = 'pool5';
 featuresTrain = activations(net,augimdsTrain,layer,'OutputAs','rows');
@@ -62,15 +110,55 @@ featuresTest = activations(net,augimdsTest,layer,'OutputAs','rows');
 featuresValidate = activations(net,augimdsValidation,layer,'OutputAs','rows');
 
 %% Checkpoint save
-save('features.mat');
+save('pool5features.mat');
+
+%% Feature Extraction with early activation layer 1: res3b3_relu features
+
+layer = 'res3b3_relu';
+featuresTrain = squeeze(mean(activations(net,augimdsTrain,layer),[1 2]))';
+featuresTest = squeeze(mean(activations(net,augimdsTest,layer),[1 2]))';
+featuresValidate = squeeze(mean(activations(net,augimdsValidation,layer),[1 2]))';
+
+%% Checkpoint save
+save('res3b3features.mat');
+
+%% Feature Extraction with early activation layer 2: res4b6_relu features
+
+layer = 'res4b6_relu';
+featuresTrain = squeeze(mean(activations(net,augimdsTrain,layer),[1 2]))';
+featuresTest = squeeze(mean(activations(net,augimdsTest,layer),[1 2]))';
+featuresValidate = squeeze(mean(activations(net,augimdsValidation,layer),[1 2]))';
+
+%% Checkpoint save
+save('res4b6features.mat');
+
+%% Feature Extraction with early activation layer 3: res4b12_relu features
+
+layer = 'res4b12_relu';
+featuresTrain = squeeze(mean(activations(net,augimdsTrain,layer),[1 2]))';
+featuresTest = squeeze(mean(activations(net,augimdsTest,layer),[1 2]))';
+featuresValidate = squeeze(mean(activations(net,augimdsValidation,layer),[1 2]))';
+
+%% Checkpoint save
+save('res4b12features.mat');
+
+%% Feature Extraction with early activation layer 4: res4b18_relu features
+
+layer = 'res4b18_relu';
+featuresTrain = squeeze(mean(activations(net,augimdsTrain,layer),[1 2]))';
+featuresTest = squeeze(mean(activations(net,augimdsTest,layer),[1 2]))';
+featuresValidate = squeeze(mean(activations(net,augimdsValidation,layer),[1 2]))';
+
+%% Checkpoint save
+save('res4b18features.mat');
 
 %% Checkpoint load
-load('features.mat')
+%load('pool5features.mat')
 
 
 %% Train multi-class SVM
-classifier = fitcecoc(featuresTrain,YTrain);
+classifier = fitcecoc(featuresTrain,trainingLabels);
 
 YPred = predict(classifier,featuresTest);
-accuracy = mean(YPred == YTest)
+accuracy = mean(YPred == testingLabels)
 
